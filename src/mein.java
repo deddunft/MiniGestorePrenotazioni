d51c6b29bcb368796e8b2810dd7a7201a6c958e1
deddunft <luca36italia@gmail.com>
2022-03-12 12:01:19 +0100

    vai nenno

--- src/mein.java
+++ src/mein.java
@@ -3,12 +3,14 @@ public class mein {
     public static void main(String[] args) {
         MiniGestorePrenotazioni mgp1 = new MiniGestorePrenotazioni(5,5);
         PrenotazioneSingola p1 ;
-        Contatore count = new Contatore();
-        System.out.println(mgp1.salae.getPosti());
-        prenotazioneG pg2 = new prenotazioneG(Sala.INTERNO,3, count.getCont());
+        Contatore counti = new Contatore();
+        Contatore counte = new Contatore();
 
-        prenotazioneG pg1 = new prenotazioneG(Sala.ESTERNO,2, count.Conta());
-        System.out.println(mgp1.Prenota(p1=new PrenotazioneSingola(Sala.INTERNO, count.Conta())));
+        System.out.println(mgp1.salae.getPosti());
+        prenotazioneG pg2 = new prenotazioneG(Sala.INTERNO,3, counti.getCont());
+
+        prenotazioneG pg1 = new prenotazioneG(Sala.ESTERNO,2, counte.getCont());
+        System.out.println(mgp1.Prenota(p1=new PrenotazioneSingola(Sala.INTERNO, counti.Conta())));
         System.out.println(mgp1.salae.getPosti());
         System.out.println(mgp1.Prenota(pg1));
         System.out.println(mgp1.salae.getPosti());
@@ -17,6 +19,8 @@ public class mein {
         System.out.println(mgp1);
         System.out.println(mgp1.salae.getPosti());
         System.out.println(mgp1.Prenota(pg2));
+        System.out.println(mgp1);
+        mgp1.terminaPrenotazione(pg2);
         System.out.println(mgp1);
 
     }
